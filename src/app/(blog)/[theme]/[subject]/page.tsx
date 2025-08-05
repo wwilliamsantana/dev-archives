a764a0b47d980182949bcec264c780e547efd3ed
William Santana <euwilliamsantana@gmail.com>
2025-08-05 10:44:30 -0300

    refactor: Page Subject: Remove Carousel, Add GRID LAYOUT

--- src/app/(blog)/[theme]/[subject]/page.tsx
+++ src/app/(blog)/[theme]/[subject]/page.tsx
@@ -1,9 +1,3 @@
-import {
-  Carousel,
-  CarouselContent,
-  CarouselItem,
-} from '@/components/ui/carousel'
-
 import Link from 'next/link'
 import {
   Breadcrumb,
@@ -60,23 +54,19 @@ export default async function Subject({ params }: SubjectParamsProps) {
         </BreadcrumbList>
       </Breadcrumb>
 
-      <section className="flex flex-col gap-4">
-        <Carousel className="w-full">
-          <CarouselContent className="flex gap-2 ml-1">
-            {data?.content.map((post) => {
-              return (
-                <Link
-                  key={post.title}
-                  href={`/${encodeURIComponent(theme)}/${encodeURIComponent(subject)}/${encodeURIComponent(post.title)}`}
-                >
-                  <CarouselItem className="w-[200px] h-[110px] p-9 bg-stone-800 rounded text-white text-center max-md:basis-1/3 max-sm:basis-1/2 flex items-center justify-center">
-                    {post.title}
-                  </CarouselItem>
-                </Link>
-              )
-            })}
-          </CarouselContent>
-        </Carousel>
+      <section className="grid grid-cols-5 gap-3 max-xl:grid-cols-4 max-lg:grid-cols-3 max-md:grid-cols-2 max-sm:grid-cols-1 gap-y-3">
+        {data?.content.map((post) => {
+          return (
+            <Link
+              key={post.title}
+              href={`/${encodeURIComponent(theme)}/${encodeURIComponent(subject)}/${encodeURIComponent(post.title)}`}
+            >
+              <div className="w-[200px] h-[110px] p-9 bg-stone-800 rounded text-white text-center max-md:basis-1/3 max-sm:basis-1/2 flex items-center justify-center">
+                {post.title}
+              </div>
+            </Link>
+          )
+        })}
       </section>
     </main>
   )
